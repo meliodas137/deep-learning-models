bee97df24008914d34244fee7eebe56446cc1676
meliodas137 <mayankgt1043@gmail.com>
2023-04-08 21:54:23 -0400

    created a 2-layer model using torch and initing caches for grad decent

--- models/MLP/mlp.py
+++ models/MLP/mlp.py
@@ -1,2 +1,40 @@
+import torch
+import torch.nn as nn
+import numpy as np
 class MLP:
-    pass
+    def __init__(
+        self,
+        linear_1_in_features,
+        linear_1_out_features,
+        f_function,
+        linear_2_in_features,
+        linear_2_out_features,
+        g_function
+    ):
+        """
+        Args:
+            linear_1_in_features: the in features of first linear layer
+            linear_1_out_features: the out features of first linear layer
+            linear_2_in_features: the in features of second linear layer
+            linear_2_out_features: the out features of second linear layer
+            f_function: string for the f function: relu | sigmoid | identity
+            g_function: string for the g function: relu | sigmoid | identity
+        """
+        self.f_function = f_function
+        self.g_function = g_function
+
+        self.parameters = dict(
+            W1 = torch.randn(linear_1_out_features, linear_1_in_features),
+            b1 = torch.randn(linear_1_out_features),
+            W2 = torch.randn(linear_2_out_features, linear_2_in_features),
+            b2 = torch.randn(linear_2_out_features),
+        )
+        self.grads = dict(
+            dJdW1 = torch.zeros(linear_1_out_features, linear_1_in_features),
+            dJdb1 = torch.zeros(linear_1_out_features),
+            dJdW2 = torch.zeros(linear_2_out_features, linear_2_in_features),
+            dJdb2 = torch.zeros(linear_2_out_features),
+        )
+
+        # put all the cache value you need in self.cache
+        self.cache = dict()
